09fed92fb5257809eeff4acfc423b4cc1779c512
pa1nx9 <90489219+pa1nx9@users.noreply.github.com>
2021-12-23 12:10:36 +0600

    Create Redeem codes.lua

---
+++ Redeem codes.lua
@@ -0,0 +1,32 @@
+wait(0.5)
+local A_1 = "magic100k"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "sulley300k"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "sulley100k"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "super75k"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "attackofgiants"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "awesome50k"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "bronzepiece_"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
+wait(0.5)
+local A_1 = "realdaireb"
+local Event = game:GetService("ReplicatedStorage").Remote.RedeemCode
+Event:InvokeServer(A_1)
